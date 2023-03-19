20ff4064a468e284c5aae89ca5d161e3a628aab0
Andi Syafrianda <andisyafrianda@gmail.com>
2023-03-19 23:28:08 +0700

    add empty placeholder index.tsx

---
+++ src/components/CategoryCard/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Empt = () => {
+    return <></>;
+    };
+
+export default Empt;
---
+++ src/pages/admin/articles/[articleId]/index.tsx
@@ -0,0 +1,10 @@
+// index.tsx this empty react
+
+import React from 'react';
+
+const Empt = () => {
+    return <></>;
+    };
+
+export default Empt;
+
